2e0ade9d3d2b2f0e1b4ebe966fbc6b719b2f7223
jaweriashabbir <jaweriamalik0677@gmail.com>
2024-10-05 20:35:05 +0500

    Film Recommender

---
+++ film recommender.py
@@ -0,0 +1,11 @@
+genre = (input('Enter about your genre:'))
+if genre == 'comedy':
+    print('1_3 idiots 2_pagal panti  3_murder mubarak 4_jackpot 5_nam aloom afraad 6_Quiad-e-azam zindabad')
+elif genre == 'sci fiction':
+    print('1_bermuda tentacles 2_annihilation 3_kalki 4_the soul 5_the mummy')
+elif genre == 'action':
+    print('1_fighter 2_salaar 3_tiger 4_savi 5_jailer')
+elif genre == 'thriller':
+    print('1_sector 2_speak no evil 3_trap 4_ wolfs 5_joker 6_hacker')
+elif genre == 'horror':
+    print('1_Evil dead rise 2_hostel 3_the witch 4_shaitaan 5_kanchna 6_bhool bholiyyan 2')
