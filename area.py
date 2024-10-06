a851d77027b4b2601de10ad9033375f339c9643d
jaweriashabbir <jaweriamalik0677@gmail.com>
2024-10-06 20:14:02 +0500

    Finding Area
    You can easily find the area of Square, Rectangle, Circle and Triangle through this program

---
+++ area.py
@@ -0,0 +1,18 @@
+# area of square
+len = 5
+square_area = (len*len)
+print(square_area)
+# area of rectangle
+width = 3
+rectangle_area = (len*width)
+print(rectangle_area)
+# area of circle
+radius = 4
+pie = 22/7
+circle_area = (pie*radius**2)
+print(circle_area)
+# area of triangle
+b = 5
+h = 5
+triangle_area = (1/2*h*b)
+print(triangle_area)
