84ac1fafb619c1cdf1526197560a05e164989db3
jaweriashabbir <jaweriamalik0677@gmail.com>
2024-10-06 22:09:54 +0500

    Height checker
    Enter your height and check you are tall, short and average height person.

---
+++ height.py
@@ -0,0 +1,8 @@
+# Enter your height and check your height is tall ,short or average.
+height = 171
+if height > 173.4:
+    print("The person is tall")
+if height < 173.4:
+    print("The person is short")
+if height == 173.4:
+    print("person has average height")
