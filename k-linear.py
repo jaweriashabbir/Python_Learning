bf3995541a223c0a10bfc230c26e0cbf2d7b0674
jaweriashabbir <jaweriamalik0677@gmail.com>
2024-10-14 18:01:08 +0500

    K-linear
    Check string with the help of indexes.

---
+++ k-linear.py
@@ -0,0 +1,25 @@
+haystack = 'safe home '
+needle = ' safe home safe life'
+checker = ''
+condition = True
+index = -1
+# y = len(haystack)-1
+for i in range(0, len(haystack)):
+    for j in range(len(needle)):
+        try:
+            checker = checker + haystack[i + j]
+            if checker == needle:
+                index = i
+                condition = False
+        except:
+            pass
+    if condition == False:
+        break
+    checker = ''
+print(index)
+
+
+
+
+
+
